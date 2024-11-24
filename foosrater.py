c7028b35e1f05bc0b602867950536cd09d1880a7
robbertmijn <robbertmijn@gmail.com>
2024-11-24 13:54:37 +0100

    class_wip

--- foosrater.py
+++ foosrater.py
@@ -1,7 +1,17 @@
 from datetime import datetime
 from typing import List, Tuple
 
-# test
+
+def expected_outcome(opponent_elo: float, player_elo: float):
+
+    return 1 / (1 + 10 ** ((opponent_elo - player_elo) / 400))
+
+
+def elo_change(player_elo: float, opponent_elo: float, player_score: int, opponent_score: int, k: int = 64, c: int = 0):
+    
+    actual_outcome = (c + player_score) / (player_score + opponent_score)
+    return k * (actual_outcome - expected_outcome(opponent_elo, player_elo))
+
 
 class Player:
     def __init__(self, name: str, elo: int = 1000, games: list = None):
@@ -10,7 +20,7 @@ class Player:
         self.games = games if games is not None else []
 
     def __repr__(self):
-        return f"PLAYER | {self.name}, elo: {self.elo}, games: {len(self.games)}"
+        return f"PLAYER | {self.name}, elo: {self.elo}, games: {len(self.games)}\n"
 
 
 class Game:
@@ -29,7 +39,7 @@ class Game:
     def __repr__(self):
         return (f"GAME | red team: {self.red_team[0].name} & {self.red_team[1].name} vs "
                 f"blue team: {self.blue_team[0].name} & {self.blue_team[1].name} - "
-                f"{self.red_score} - {self.blue_score}, on {self.date_time}"
+                f"{self.red_score} - {self.blue_score}, on {self.date_time}\n"
                 )
 
 
@@ -62,7 +72,7 @@ class League:
             player.games.append(game)
             
         self.games.append(game)
-        # self.recalculate_elo()
+
 
     def edit_game(self, game_index: int, red_score: int, blue_score: int):
         if 0 <= game_index < len(self.games):
@@ -72,34 +82,20 @@ class League:
         else:
             print("Invalid game index")
 
-    def calculate_elo(self, red_team_elo: float, blue_team_elo: float, red_score: int, blue_score: int):
-        K = 32
-        expected_red = 1 / (1 + 10 ** ((blue_team_elo - red_team_elo) / 400))
-        expected_blue = 1 / (1 + 10 ** ((red_team_elo - blue_team_elo) / 400))
-        if red_score > blue_score:
-            red_actual, blue_actual = 1, 0
-        elif blue_score > red_score:
-            red_actual, blue_actual = 0, 1
-        else:
-            red_actual, blue_actual = 0.5, 0.5
-        
-        red_team_elo += K * (red_actual - expected_red)
-        blue_team_elo += K * (blue_actual - expected_blue)
-        return red_team_elo, blue_team_elo
 
-    def recalculate_elo(self):
+    def calculate_elo(self):
+
         for player in self.players.values():
             player.elo = 1000.0
 
         for game in self.games:
             red_team_elo = (game.red_team[0].elo + game.red_team[1].elo) / 2
             blue_team_elo = (game.blue_team[0].elo + game.blue_team[1].elo) / 2
-            red_team_elo, blue_team_elo = self.calculate_elo(red_team_elo, blue_team_elo, game.red_score, game.blue_score)
 
-            game.red_team[0].elo = red_team_elo
-            game.red_team[1].elo = red_team_elo
-            game.blue_team[0].elo = blue_team_elo
-            game.blue_team[1].elo = blue_team_elo
+            game.red_team[0].elo += elo_change(red_team_elo, blue_team_elo, game.red_score, game.blue_score)
+            game.red_team[1].elo += elo_change(red_team_elo, blue_team_elo, game.red_score, game.blue_score)
+            game.blue_team[0].elo += elo_change(blue_team_elo, red_team_elo, game.blue_score, game.red_score)
+            game.blue_team[1].elo += elo_change(blue_team_elo, red_team_elo, game.blue_score, game.red_score)
             
     def __repr__(self):
         
@@ -109,7 +105,9 @@ class League:
 league = League()
 
 league.add_game(("Robbert", "Anne"), ("Manon", "Sjoerd"), 3, 2, datetime.now())
-league.add_game(("Anne", "Manon"), ("Jasper", "Rob"), 1, 4, datetime.now())
+league.add_game(("Anne", "Manon"), ("Jasper", "Rob"), 1, 7, datetime.now())
+league.add_game(("Anne", "Jasper"), ("Robbert", "Rob"), 10, 4, datetime.now())
+league.calculate_elo()
 
 league.games
 league.players
