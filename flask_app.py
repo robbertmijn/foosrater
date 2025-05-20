4f010cdcbe8f7f66e2331bf61c50a817d212b209
Robbert van der Mijn <robbert.vandermijn@tno.nl>
2025-05-20 11:58:58 +0200

    added qr code poster

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify, s
 import json
 import os
 import csv
+import qrcode
+import io
+import base64
 
 from foosrater import League, Player
 
@@ -9,16 +12,14 @@ app = Flask(__name__)
 
 DATA_FOLDER = 'data'
 
-@app.route('/', methods=['GET', 'POST'])
-def landing():
-    return redirect(url_for('index', league_name="hmt_2025"))
+# @app.route('/', methods=['GET', 'POST'])
+# def landing():
+#     return redirect(url_for('index', league_name="hmt_2025"))
 
 
 @app.route('/<league_name>', methods=['GET', 'POST'])
 def index(league_name):
 
-
-        
     data = os.path.join(DATA_FOLDER, league_name + ".csv")
     
     if league_name == "hmt_2024":
@@ -112,6 +113,27 @@ def player_profile(league_name, player_name):
     data = league.players[player_name].get_player_profile()
     
     return jsonify(data)
+
+
+@app.route('/<league_name>/make_qr_poster')
+def make_qr_poster(league_name):
+    target_url = f"https://robbertmijn.pythonanywhere.com/{league_name}"
+
+    # Generate QR code
+    qr = qrcode.QRCode(box_size=10, border=4)
+    qr.add_data(target_url)
+    qr.make(fit=True)
+    img = qr.make_image(fill="black", back_color="white")
+
+    # Convert to base64
+    buf = io.BytesIO()
+    img.save(buf, format="PNG")
+    img_base64 = base64.b64encode(buf.getvalue()).decode('utf-8')
+
+    return render_template("qr_poster.html",
+                           league_name=league_name,
+                           target_url=target_url,
+                           img_base64=img_base64)
 
 
 @app.route('/favicon.ico')
